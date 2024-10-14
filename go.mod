86a217347189283a9fdeeec831984dd692dba953
marydanieliann <mdanieyan29@gmail.com>
2024-10-14 14:42:08 +0400

    Implementations of redis.uptrace.dev page.

---
+++ go.mod
@@ -0,0 +1,15 @@
+module redisInteraction
+
+go 1.23.2
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/redis/go-redis/v9 v9.6.1
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+)
---
+++ scanStruct.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/redis/go-redis/v9"
+)
+
+type Model struct {
+	Str1    string   `redis:"str1"`
+	Str2    string   `redis:"str2"`
+	Bytes   []byte   `redis:"bytes"`
+	Int     int      `redis:"int"`
+	Bool    bool     `redis:"bool"`
+	Ignored struct{} `redis:"-"`
+}
+
+func main() {
+	ctx := context.Background()
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+	_ = rdb.FlushDB(ctx).Err()
+
+	if _, err := rdb.Pipelined(ctx, func(rdb redis.Pipeliner) error {
+		rdb.HSet(ctx, "key", "str1", "hello")
+		rdb.HSet(ctx, "key", "str2", "world")
+		rdb.HSet(ctx, "key", "int", 123)
+		rdb.HSet(ctx, "key", "bool", 1)
+		rdb.HSet(ctx, "key", "bytes", []byte("this is bytes !"))
+		return nil
+	}); err != nil {
+		panic(err)
+	}
+
+	var model1, model2 Model
+
+	if err := rdb.HGetAll(ctx, "key").Scan(&model1); err != nil {
+		panic(err)
+	}
+
+	if err := rdb.HMGet(ctx, "key", "str1", "int").Scan(&model2); err != nil {
+		panic(err)
+	}
+
+	spew.Dump(model1)
+	spew.Dump(model2)
+}
